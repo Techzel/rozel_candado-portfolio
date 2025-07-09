d159c638ce8bedcf7c3a87a2750371ae33a3dfac
rozel <rozelcandado897>
2025-07-09 10:26:31 +0800

    changess

---
+++ js/size.js
@@ -0,0 +1,15 @@
+
+tailwind.config = {
+    
+                theme: {
+                    extend: {
+                    screens: {
+
+                        'xs': {min : '320px', max: '639px'},
+                        'sm': {min : '640px', max: '767px'},
+                        'md': {min : '768px', max: '1023px'},
+                        'lg': '1024px',        
+                    },
+                    },
+                },
+                };
